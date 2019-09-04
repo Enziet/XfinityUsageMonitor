65d2f886bdecf553f4fbac3cd5dbb701a7cf8840
Enziet <45093708+Enziet@users.noreply.github.com>
2019-09-04 16:31:16 -0700

    Added project files

---
+++ HeadlessManager.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XfinityUsageMonitor
+{
+    class HeadlessManager
+    {
+        ChromeDriver _driver;
+        ChromeOptions _options;
+
+        private bool _status = false;
+        public bool Status { get => _status; set => _status = value; }
+
+        HeadlessManager()
+        {
+            _options = new ChromeOptions();
+            _options.AddArgument("--headless");
+            _driver = new ChromeDriver(_options);
+            _status = true;
+        }
+
+        
+    }
+}
